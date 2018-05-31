a9566a67572e9d86144e9aa42fbc306dca3578c9
suishanwen <suishanwen@icloud.com>
2018-05-31 16:51:05 +0800

    U

--- ok/OKClient.py
+++ ok/OKClient.py
@@ -7,17 +7,18 @@ import tokens.Token as Token1
 import tokens.Token2 as Token2
 import api.OkexClient as Client
 import configparser
+import json
+import threading
 
 # read config
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-Token = Token1
-
-symbol = config.get("trade", "symbol")
-_type = config.get(symbol, "type")
-if _type == 'transaction':
-    Token = Token2
-
 if __name__ == '__main__':
-    Token.__main__(Client.OkexClient(), symbol)
+    symbols = json.loads(config.get("trade", "symbol"))
+    for symbol in symbols:
+        _type = config.get(symbol, "type")
+        if _type == 'transaction':
+            threading.Thread(target=Token2.__main__, args=(Client.OkexClient(), symbol,)).start()
+        else:
+            threading.Thread(target=Token1.__main__, args=(Client.OkexClient(), symbol,)).start()

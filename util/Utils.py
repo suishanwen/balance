f87a308e089aa912259c364860e44277aeb24785
suishanwen <suishanwen@icloud.com>
2018-07-10 15:38:50 +0800

    U

--- util/Utils.py
+++ util/Utils.py
@@ -14,6 +14,8 @@ import urllib
 import urllib.parse
 import urllib.request
 import requests
+from ecdsa import SigningKey
+from hashlib import sha256
 import configparser
 
 # read config
@@ -25,10 +27,20 @@ configBase.read("../key.ini")
 ACCESS_KEY = configBase.get("huobipro", "access_key")
 SECRET_KEY = configBase.get("huobipro", "secret_key")
 
-# API 请求地址
-MARKET_URL = "https://api.huobipro.com"
-TRADE_URL = "https://api.huobipro.com"
+# Need to replace with the actual value generated from below command
+# openssl ecparam -name secp256k1 -genkey -noout -out secp256k1-key.pem
+PRIVATE_KEY = '''-----BEGIN EC PRIVATE KEY-----
+    MHQCAQEEIGsZdYPWE/j9v3PyQuTP7XEJM5AmX02FCCHeIscumc9PoAcGBSuBBAAK
+oUQDQgAEH1WA/nxcZIXvRRIucEJ2viijhd7oEbzNqYJz/5gBF7381i8CPuKv06XH
+5eHglrcOW3V9r/6YByBIXn/sUd/4hw==
+    -----END EC PRIVATE KEY-----'''
 
+# API request URL
+MARKET_URL = "https://api.huobi.pro"
+TRADE_URL = "https://api.huobi.pro"
+
+# Can first request to call get_accounts()to find the target acct_id,later can just specify the actual acc_id in the api call
+ACCOUNT_ID = None
 
 
 # 'Timestamp': '2017-06-02T06:13:49'
@@ -36,8 +48,7 @@ TRADE_URL = "https://api.huobipro.com"
 def http_get_request(url, params, add_to_headers=None):
     headers = {
         "Content-type": "application/x-www-form-urlencoded",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/39.0.2171.71 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
     }
     if add_to_headers:
         headers.update(add_to_headers)
@@ -85,8 +96,9 @@ def api_key_get(params, request_path):
     host_url = TRADE_URL
     host_name = urllib.parse.urlparse(host_url).hostname
     host_name = host_name.lower()
-    params['Signature'] = create_sign(params, method, host_name, request_path, SECRET_KEY)
-
+    signature = createSign(params, method, host_name, request_path, SECRET_KEY)
+    params['Signature'] = signature
+    params['PrivateSignature'] = createPrivateSignature(signature, PRIVATE_KEY)
     url = host_url + request_path
     return http_get_request(url, params)
 
@@ -102,13 +114,15 @@ def api_key_post(params, request_path):
     host_url = TRADE_URL
     host_name = urllib.parse.urlparse(host_url).hostname
     host_name = host_name.lower()
-    params_to_sign['Signature'] = create_sign(params_to_sign, method, host_name, request_path, SECRET_KEY)
+    signature = createSign(params_to_sign, method, host_name, request_path, SECRET_KEY)
+    params_to_sign['Signature'] = signature
+    params_to_sign['PrivateSignature'] = createPrivateSignature(signature, PRIVATE_KEY)
     url = host_url + request_path + '?' + urllib.parse.urlencode(params_to_sign)
     return http_post_request(url, params)
 
 
-def create_sign(params, method, host_url, request_path, secret_key):
-    sorted_params = sorted(params.items(), key=lambda d: d[0], reverse=False)
+def createSign(pParams, method, host_url, request_path, secret_key):
+    sorted_params = sorted(pParams.items(), key=lambda d: d[0], reverse=False)
     encode_params = urllib.parse.urlencode(sorted_params)
     payload = [method, host_url, request_path, encode_params]
     payload = '\n'.join(payload)
@@ -119,3 +133,9 @@ def create_sign(params, method, host_url, request_path, secret_key):
     signature = base64.b64encode(digest)
     signature = signature.decode()
     return signature
+
+
+def createPrivateSignature(signature, private_key):
+    signingKey = SigningKey.from_pem(private_key, hashfunc=sha256)
+    privateSignature = signingKey.sign(signature.encode(encoding='UTF8'))
+    return base64.b64encode(privateSignature)

22382cec8a22eb5edf9916e6f39096c03d2afa22
suishanwen <suishanwen@icloud.com>
2019-04-16 16:35:18 +0800

    test

--- example/websocket_example.py
+++ example/websocket_example.py
@@ -52,5 +52,5 @@ async def subscribe_without_login(url, channels):
 
 
 url = 'wss://real.okex.com:10442/ws/v3'
-channels = ["spot/depth5:OKB-USDT"]
+channels = ["spot/depth5:okb_usdt"]
 asyncio.get_event_loop().run_until_complete(subscribe_without_login(url, channels))

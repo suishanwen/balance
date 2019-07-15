eed5edda1b4048134bd4c1a82dd5e7f8d0f9d96a
suishanwen <suishanwen@icloud.com>
2019-07-15 16:41:02 +0800

    bug fix

--- codegen/generator.py
+++ codegen/generator.py
@@ -11,15 +11,15 @@ def enc(path):
 
 def dec(path):
     with open(path, 'r') as f:
+        data = f.read()
         try:
-            return str(decrypt(prv_key, bytes.fromhex(f.read())), encoding="utf-8")
+            return str(decrypt(prv_key, bytes.fromhex(data)), encoding="utf-8")
         except Exception as e:
             print(str(e))
-            return f.read()
+            return data
 
 
 def write(_type, path):
     code = dec(path) if _type == "dec" else enc(path)
     with open(path, 'w') as f:
         f.write(code)
-
